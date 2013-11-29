9575d228fc61aec1b4f0550d1854e421d564dc27
zorksylar <pin.gao2008@gmail.com>
2013-11-30 02:29:19 +0800

    fixd some bug

---
+++ main.py
@@ -0,0 +1,39 @@
+
+from pprocess import Pprocess as PP
+from plsa import pLSA 
+import numpy as np
+import logging
+
+
+if __name__ == '__main__':
+  logging.basicConfig(filename='plsa.log', level=logging.INFO)
+  console = logging.StreamHandler()
+  console.setLevel(logging.INFO)
+  logging.getLogger('').addHandler(console)
+
+  fname = './data.txt'
+  fsw = './stopwords.txt'
+  eps = 20.0
+  key_word_size = 10
+
+  pp = PP(fname, fsw)
+  t_d = pp.get_t_d()
+
+  V,D = t_d.shape
+  logging.info("V = %d  D = %d" %(V,D))
+  #print "V = %d  D = %d" %(V,D)
+
+  pmodel = pLSA()
+  for z in range(3,(D+1)/2):
+    (l, p_d_z, p_w_z, p_z)  = pmodel.train(t_d, z, eps)
+    logging.info('z = %d eps = %f' %(z, l))
+    for itz in range(z):
+      logging.info('Topic %d' %itz)
+      data = [(p_w_z[i][itz], i)  for i in range(len(p_w_z[:itz])) ]
+      data.sort(key=lambda tup:tup[0], reverse=True)
+      for i in range(key_word_size):
+        logging.info('%s : %.6f ' %(pp.get_word(data[i][1]), data[i][0] ))
+
+
+
+
